f4870a950652a90666cd94b1daf4a474a0b28681
MihaelaPapa <mihaela.papa@gmail.com>
2019-01-24 07:40:20 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#Learn Python 3 The Hardway
+
+#Exercise 1
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not.'")
+print(' "said" do not touch this.')
+print("And another line!")
---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your program laterself.
+# Anything after the # is ignored by Phyton.
+
+print ("I could hace code like this") # and the comment after is ignoredself.
+
+# You can also use a comment to "disable" or comment out code
+
+# print ("This won't run.")
+print("This will run.")
+
+print("Hi # there.")
