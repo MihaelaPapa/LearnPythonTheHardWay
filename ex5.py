f135dc327db85a27be97d87cda2485997ac204fa
Ladybug <ladybug@MacBook-Pro-3.local>
2019-01-27 11:47:32 +0100

    Exercise 5.

---
+++ ex5.py
@@ -0,0 +1,21 @@
+# Learn Python 3 the Hard Way
+# Exercise 4
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'white'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("That is actually not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print("His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight} I get {total}.")
