ad4c21b295d2dee468bc11c7b01423e3811ca00c
Ladybug <ladybug@MacBook-Pro-3.local>
2019-01-29 07:33:02 +0100

    Exercise 12.

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# Learn Python 3 the Hard Way
+# Exercise 11
+
+#print("Hello!")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So you are {age} old, {height} tall and {weight} heavy.")
